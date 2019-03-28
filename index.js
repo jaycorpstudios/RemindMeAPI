49fe93438a7c204165de0e81b3ba38a719484f3e
jaycorpstudios <jacorpstudios@me.com>
2019-03-27 23:37:37 -0600

    Update deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-app.set('port', process.env.PORT || 7777);
+// app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
