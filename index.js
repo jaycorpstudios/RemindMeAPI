7bfcfa0d326c51dfdda00a5824147909b9688c08
jaycorpstudios <jacorpstudios@me.com>
2019-03-27 23:33:16 -0600

    export app

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+module.exports = server
